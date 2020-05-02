af74872405275c70dade411d27ebbd53811d0255
Myles Borins <myles.borins@gmail.com>
2020-05-02 01:35:08 -0400

    chore: more conversion

--- index.mjs
+++ index.mjs
@@ -1,25 +1,31 @@
 import {createRequire} from 'module';
+import {join} from 'path';
+import adventure from 'workshopper-adventure';
+
+import header from 'workshopper-adventure/default/header.js'
+import footer from './lib/footer.js';
+
+import problem from  './lib/problem.js'
 
 const require = createRequire(import.meta.url);
+const dirname = new URL('./', import.meta.url).pathname;
 
-const problem = require('./lib/problem')
-
-var jsing = require('workshopper-adventure')({
-  appDir: new URL('./', import.meta.url).pathname,
+const esmoduling = adventure({
+  appDir: dirname,
   languages: ['en'],
-  header: require('workshopper-adventure/default/header'),
-  footer: require('./lib/footer.js')
-})
+  header,
+  footer
+});
 
-jsing.addAll(require('./menu.json').map(function (name) {
+esmoduling.addAll(require('./menu.json').map(function (name) {
   return {
     name,
     fn: function () {
       var p = name.toLowerCase().replace(/\s/g, '-')
-      var dir = require('path').join(__dirname, 'problems', p)
+      var dir = join(dirname, 'problems', p)
       return problem(dir)
     }
   }
 }))
 
-export default jsing;
+export default esmoduling;
